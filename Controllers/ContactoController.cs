bfb6fb5eaae8f5b52ffa0f505bfed5e8b473b69f
Fred Duarte <fduartej@gmail.com>
2022-04-02 11:34:59 -0500

    Mostrando Grabacion de Contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using appcomics.Models;
+using appcomics.Data;
 namespace appcomics.Controllers
 {
     public class ContactoController: Controller
     {
         private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public ContactoController(ILogger<ContactoController> logger)
+        public ContactoController(ApplicationDbContext context,
+            ILogger<ContactoController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
@@ -21,8 +25,12 @@ namespace appcomics.Controllers
             return View();
         }
 
-        public IActionResult Create()
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
         {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "Se registro el contacto";
             return View("Index");
         }
 
